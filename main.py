7d2f76bed679ad3e4cf9cb360c118d0a84b81840
Haker <noyemkaren@gmail.com>
2026-02-17 16:12:51 +0300

    Project: 0.63v

--- main.py
+++ main.py
@@ -3,56 +3,55 @@ from fastapi.templating import Jinja2Templates
 from fastapi.responses import RedirectResponse
 from database import add_user, get_all_users, delete_user
 
-
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
-def main(request: Request):
+async def main(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
 
 @app.get("/index.html")
-def gabon(request: Request):
+async def gabon(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.get("/armenia.html")
-def armenia(request: Request):
+async def armenia(request: Request):
     return templates.TemplateResponse("armenia.html", {"request": request})
 
 @app.get("/rusha.html")
-def rusha(request: Request):
+async def rusha(request: Request):
     return templates.TemplateResponse("rusha.html", {"request": request})
 
 @app.get("/american.html")
-def america(request: Request):
+async def america(request: Request):
     return templates.TemplateResponse("american.html", {"request": request})
 
 @app.get("/indi.html")
-def indi(request: Request):
+async def indi(request: Request):
     return templates.TemplateResponse("indi.html", {"request": request})
 
 @app.get("/city.html")
-def city(request: Request):
+async def city(request: Request):
     return templates.TemplateResponse("city.html", {"request": request})
 
 @app.get("/phil.html")
-def phil(request: Request):
+async def phil(request: Request):
     return templates.TemplateResponse("phil.html", {"request": request})
 
 @app.get("/sh.html")
-def sh(request: Request):
+async def sh(request: Request):
     return templates.TemplateResponse("sh.html", {"request": request})
 
 @app.get("/currencies.html")
-def curr(request: Request):
+async def curr(request: Request):
     return templates.TemplateResponse("currencies.html", {"request": request})
 
 @app.get("/create.html")
-def create_page(request: Request):
+async def create_page(request: Request):
     return templates.TemplateResponse("create.html", {"request": request})
 
 @app.post("/add-photo")
-def add_photo(request: Request, image_url: str = Form(...)):
+async def add_photo(image_url: str = Form(...)):
     all_photos = get_all_users()
     new_id = 1
     if all_photos:
@@ -61,15 +60,14 @@ def add_photo(request: Request, image_url: str = Form(...)):
     return RedirectResponse(url="/instagram.html", status_code=303)
 
 @app.get("/instagram.html")
-def insta(request: Request):
+async def insta(request: Request):
     all_photos = get_all_users()
     return templates.TemplateResponse("instagram.html", {"request": request, "photos": all_photos})
 
 @app.get("/delete/{id}")
-def dele(id: int):
-    delete_user(id)
+async def dele(user_id: int):
+    delete_user(user_id)
     return RedirectResponse(url="/instagram.html", status_code=303)
-
 
 @app.get("/Запрос_на_создания_фото_автору_нажмите_try_it_out:_затем_execute", tags=["Запос_Автору_на_добавления_фото_в_2_основные_категории"], summary="Добавить фото в категории девочки/мальчики")
 def giter(photo_url: str, categoria: str):
